a6b923cc69c09ef19c2f06f6e53b7c50ba9aa977
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-05-02 18:12:00 +0300

    realize conection to database

--- Helsi/Authorization.cs
+++ Helsi/Authorization.cs
@@ -1,4 +1,11 @@
 using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic.ApplicationServices;
+
+
 
 namespace Helsi
 {
@@ -17,14 +24,57 @@ namespace Helsi
         private void loginInDatabase_button_Click(object sender, EventArgs e)
         {
             string login = login_textBox.Text;
-            string password = password_textBox.Text;
+            string password = password_textBox.Text;            
 
-            if (login == "asd" && password == "asd" || password == "")
+            string pathToConectionString = Application.StartupPath.ToString();
+            pathToConectionString += "/" + "ConectionString.json";
+
+
+            string ConectionString = "";
+
+
+            var options = new JsonSerializerOptions
             {
-                var mainForm = new MainForm();
-                mainForm.Show();
-                this.Hide();
+                WriteIndented = true, // додає відступи для читаємості
+            };
+
+            if (!File.Exists(pathToConectionString))
+            {
+                using (FileStream file = new FileStream(pathToConectionString, FileMode.Create))
+                {
+                    JsonSerializer.Serialize(file, ConectionString, options);
+                }
             }
+            else
+            {
+                using (FileStream file = new FileStream(pathToConectionString, FileMode.Open))
+                {
+                    ConectionString = JsonSerializer.Deserialize<string>(file);
+                }
+            }
+           
+            using (SqlConnection connection = new SqlConnection(ConectionString + "User Id =" + login + ";" + "Password =" + password + ";"))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Підключення успішне!");
+                    var mainForm = new MainForm();
+                    mainForm.Show();
+                    this.Hide();
+                    connection.Close();
+
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("Не вдалося підключитися до бази даних:");              
+                }
+                
+            }
+
+           
+               
+            
         }
 
 
