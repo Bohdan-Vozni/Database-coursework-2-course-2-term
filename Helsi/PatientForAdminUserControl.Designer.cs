db8e04c59135fbad7c50ce0b3c5df73c6ee63d9f
Bohdan-Vozni <ipz23-b.voznyi@nubip.edu.ua>
2025-05-04 21:15:40 +0300

    Already was done patient for Admin without search

--- Helsi/PatientForAdminUserControl.Designer.cs
+++ Helsi/PatientForAdminUserControl.Designer.cs
@@ -28,9 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button3 = new Button();
-            button2 = new Button();
-            button1 = new Button();
+            deletePatientForAdmin_button = new Button();
+            addPatientForAdmin_button = new Button();
             addressTextBox_PatientForAdmin = new TextBox();
             label6 = new Label();
             phoneNumberTextBox_PatientForAdmin = new TextBox();
@@ -39,47 +38,41 @@
             label4 = new Label();
             fulnameTextBox_PatientForAdmin = new TextBox();
             label3 = new Label();
+            label1 = new Label();
+            findPatientForAdmin_Button = new Button();
+            patientForAdmin_dataGridView = new DataGridView();
             idTextBox_PatientForAdmin = new TextBox();
             label2 = new Label();
-            label1 = new Label();
-            showAllDataPatientForAdmin_Button = new Button();
-            patientForAdmin_dataGridView = new DataGridView();
+            updatePatientForAdmin_button = new Button();
+            findTextBox_PatientForAdmin = new TextBox();
             ((System.ComponentModel.ISupportInitialize)patientForAdmin_dataGridView).BeginInit();
             SuspendLayout();
             // 
-            // button3
+            // deletePatientForAdmin_button
             // 
-            button3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
-            button3.Location = new Point(16, 468);
-            button3.Name = "button3";
-            button3.Size = new Size(360, 32);
-            button3.TabIndex = 29;
-            button3.Text = "Видалити";
-            button3.UseVisualStyleBackColor = true;
+            deletePatientForAdmin_button.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            deletePatientForAdmin_button.Location = new Point(15, 405);
+            deletePatientForAdmin_button.Name = "deletePatientForAdmin_button";
+            deletePatientForAdmin_button.Size = new Size(360, 32);
+            deletePatientForAdmin_button.TabIndex = 29;
+            deletePatientForAdmin_button.Text = "Видалити";
+            deletePatientForAdmin_button.UseVisualStyleBackColor = true;
+            deletePatientForAdmin_button.Click += deletePatientForAdmin_button_Click;
             // 
-            // button2
+            // addPatientForAdmin_button
             // 
-            button2.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
-            button2.Location = new Point(16, 428);
-            button2.Name = "button2";
-            button2.Size = new Size(360, 34);
-            button2.TabIndex = 28;
-            button2.Text = "Оновити";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            button1.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
-            button1.Location = new Point(16, 386);
-            button1.Name = "button1";
-            button1.Size = new Size(360, 36);
-            button1.TabIndex = 27;
-            button1.Text = "Додати";
-            button1.UseVisualStyleBackColor = true;
+            addPatientForAdmin_button.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            addPatientForAdmin_button.Location = new Point(15, 323);
+            addPatientForAdmin_button.Name = "addPatientForAdmin_button";
+            addPatientForAdmin_button.Size = new Size(360, 36);
+            addPatientForAdmin_button.TabIndex = 27;
+            addPatientForAdmin_button.Text = "Додати";
+            addPatientForAdmin_button.UseVisualStyleBackColor = true;
+            addPatientForAdmin_button.Click += addPatientForAdmin_button_Click;
             // 
             // addressTextBox_PatientForAdmin
             // 
-            addressTextBox_PatientForAdmin.Location = new Point(16, 347);
+            addressTextBox_PatientForAdmin.Location = new Point(15, 284);
             addressTextBox_PatientForAdmin.Name = "addressTextBox_PatientForAdmin";
             addressTextBox_PatientForAdmin.Size = new Size(360, 23);
             addressTextBox_PatientForAdmin.TabIndex = 26;
@@ -88,7 +81,7 @@
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label6.Location = new Point(16, 319);
+            label6.Location = new Point(15, 256);
             label6.Name = "label6";
             label6.Size = new Size(79, 25);
             label6.TabIndex = 25;
@@ -96,7 +89,7 @@
             // 
             // phoneNumberTextBox_PatientForAdmin
             // 
-            phoneNumberTextBox_PatientForAdmin.Location = new Point(16, 289);
+            phoneNumberTextBox_PatientForAdmin.Location = new Point(15, 226);
             phoneNumberTextBox_PatientForAdmin.Name = "phoneNumberTextBox_PatientForAdmin";
             phoneNumberTextBox_PatientForAdmin.Size = new Size(360, 23);
             phoneNumberTextBox_PatientForAdmin.TabIndex = 24;
@@ -105,7 +98,7 @@
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label5.Location = new Point(16, 261);
+            label5.Location = new Point(15, 198);
             label5.Name = "label5";
             label5.Size = new Size(170, 25);
             label5.TabIndex = 23;
@@ -113,7 +106,7 @@
             // 
             // dateBithTextBox_PatientForAdmin
             // 
-            dateBithTextBox_PatientForAdmin.Location = new Point(16, 228);
+            dateBithTextBox_PatientForAdmin.Location = new Point(15, 165);
             dateBithTextBox_PatientForAdmin.Name = "dateBithTextBox_PatientForAdmin";
             dateBithTextBox_PatientForAdmin.Size = new Size(360, 23);
             dateBithTextBox_PatientForAdmin.TabIndex = 22;
@@ -122,7 +115,7 @@
             // 
             label4.AutoSize = true;
             label4.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label4.Location = new Point(16, 200);
+            label4.Location = new Point(15, 137);
             label4.Name = "label4";
             label4.Size = new Size(180, 25);
             label4.TabIndex = 21;
@@ -130,7 +123,7 @@
             // 
             // fulnameTextBox_PatientForAdmin
             // 
-            fulnameTextBox_PatientForAdmin.Location = new Point(16, 171);
+            fulnameTextBox_PatientForAdmin.Location = new Point(15, 108);
             fulnameTextBox_PatientForAdmin.Name = "fulnameTextBox_PatientForAdmin";
             fulnameTextBox_PatientForAdmin.Size = new Size(360, 23);
             fulnameTextBox_PatientForAdmin.TabIndex = 20;
@@ -139,69 +132,92 @@
             // 
             label3.AutoSize = true;
             label3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label3.Location = new Point(16, 143);
+            label3.Location = new Point(15, 80);
             label3.Name = "label3";
             label3.Size = new Size(45, 25);
             label3.TabIndex = 19;
             label3.Text = "ПІБ";
             // 
-            // idTextBox_PatientForAdmin
-            // 
-            idTextBox_PatientForAdmin.Location = new Point(16, 109);
-            idTextBox_PatientForAdmin.Name = "idTextBox_PatientForAdmin";
-            idTextBox_PatientForAdmin.Size = new Size(360, 23);
-            idTextBox_PatientForAdmin.TabIndex = 18;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label2.Location = new Point(16, 81);
-            label2.Name = "label2";
-            label2.Size = new Size(33, 25);
-            label2.TabIndex = 17;
-            label2.Text = "ІД";
-            // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 204);
-            label1.Location = new Point(329, 43);
+            label1.Location = new Point(329, 42);
             label1.Name = "label1";
             label1.Size = new Size(213, 30);
             label1.TabIndex = 15;
             label1.Text = "Робота з пацієнтом";
             // 
-            // showAllDataPatientForAdmin_Button
+            // findPatientForAdmin_Button
             // 
-            showAllDataPatientForAdmin_Button.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
-            showAllDataPatientForAdmin_Button.Location = new Point(16, 506);
-            showAllDataPatientForAdmin_Button.Name = "showAllDataPatientForAdmin_Button";
-            showAllDataPatientForAdmin_Button.Size = new Size(360, 32);
-            showAllDataPatientForAdmin_Button.TabIndex = 30;
-            showAllDataPatientForAdmin_Button.Text = "Показати всі записи";
-            showAllDataPatientForAdmin_Button.UseVisualStyleBackColor = true;
-            showAllDataPatientForAdmin_Button.Click += showAllDataPatientForAdmin_Button_Click;
+            findPatientForAdmin_Button.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            findPatientForAdmin_Button.Location = new Point(771, 108);
+            findPatientForAdmin_Button.Name = "findPatientForAdmin_Button";
+            findPatientForAdmin_Button.Size = new Size(152, 32);
+            findPatientForAdmin_Button.TabIndex = 30;
+            findPatientForAdmin_Button.Text = "Знайти";
+            findPatientForAdmin_Button.UseVisualStyleBackColor = true;
+            findPatientForAdmin_Button.Click += findPatientForAdmin_Button_Click;
             // 
             // patientForAdmin_dataGridView
             // 
             patientForAdmin_dataGridView.BackgroundColor = SystemColors.ButtonHighlight;
             patientForAdmin_dataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            patientForAdmin_dataGridView.Location = new Point(382, 88);
+            patientForAdmin_dataGridView.Location = new Point(381, 149);
             patientForAdmin_dataGridView.Name = "patientForAdmin_dataGridView";
             patientForAdmin_dataGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
-            patientForAdmin_dataGridView.Size = new Size(542, 450);
+            patientForAdmin_dataGridView.Size = new Size(542, 413);
             patientForAdmin_dataGridView.TabIndex = 16;
             patientForAdmin_dataGridView.CellContentClick += patientForAdmin_dataGridView_CellContentClick;
+            // 
+            // idTextBox_PatientForAdmin
+            // 
+            idTextBox_PatientForAdmin.Location = new Point(15, 42);
+            idTextBox_PatientForAdmin.Name = "idTextBox_PatientForAdmin";
+            idTextBox_PatientForAdmin.Size = new Size(360, 23);
+            idTextBox_PatientForAdmin.TabIndex = 32;
+            idTextBox_PatientForAdmin.Visible = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label2.Location = new Point(15, 14);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 25);
+            label2.TabIndex = 31;
+            label2.Text = "ПІБ";
+            label2.Visible = false;
+            // 
+            // updatePatientForAdmin_button
+            // 
+            updatePatientForAdmin_button.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            updatePatientForAdmin_button.Location = new Point(15, 363);
+            updatePatientForAdmin_button.Name = "updatePatientForAdmin_button";
+            updatePatientForAdmin_button.Size = new Size(360, 36);
+            updatePatientForAdmin_button.TabIndex = 33;
+            updatePatientForAdmin_button.Text = "Оновити";
+            updatePatientForAdmin_button.UseVisualStyleBackColor = true;
+            updatePatientForAdmin_button.Click += updatePatientForAdmin_button_Click_1;
+            // 
+            // findTextBox_PatientForAdmin
+            // 
+            findTextBox_PatientForAdmin.Location = new Point(390, 115);
+            findTextBox_PatientForAdmin.Name = "findTextBox_PatientForAdmin";
+            findTextBox_PatientForAdmin.Size = new Size(360, 23);
+            findTextBox_PatientForAdmin.TabIndex = 34;
             // 
             // PatientForAdminUserControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            Controls.Add(showAllDataPatientForAdmin_Button);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            Controls.Add(findTextBox_PatientForAdmin);
+            Controls.Add(updatePatientForAdmin_button);
+            Controls.Add(idTextBox_PatientForAdmin);
+            Controls.Add(label2);
+            Controls.Add(findPatientForAdmin_Button);
+            Controls.Add(deletePatientForAdmin_button);
+            Controls.Add(addPatientForAdmin_button);
             Controls.Add(addressTextBox_PatientForAdmin);
             Controls.Add(label6);
             Controls.Add(phoneNumberTextBox_PatientForAdmin);
@@ -210,8 +226,6 @@
             Controls.Add(label4);
             Controls.Add(fulnameTextBox_PatientForAdmin);
             Controls.Add(label3);
-            Controls.Add(idTextBox_PatientForAdmin);
-            Controls.Add(label2);
             Controls.Add(patientForAdmin_dataGridView);
             Controls.Add(label1);
             Name = "PatientForAdminUserControl";
@@ -224,9 +238,8 @@
 
         #endregion
 
-        private Button button3;
-        private Button button2;
-        private Button button1;
+        private Button deletePatientForAdmin_button;
+        private Button addPatientForAdmin_button;
         private TextBox addressTextBox_PatientForAdmin;
         private Label label6;
         private TextBox phoneNumberTextBox_PatientForAdmin;
@@ -235,10 +248,12 @@
         private Label label4;
         private TextBox fulnameTextBox_PatientForAdmin;
         private Label label3;
-        private TextBox idTextBox_PatientForAdmin;
-        private Label label2;
         private DataGridView patientForAdmin_dataGridView;
         private Label label1;
-        private Button showAllDataPatientForAdmin_Button;
+        private Button findPatientForAdmin_Button;
+        private TextBox idTextBox_PatientForAdmin;
+        private Label label2;
+        private Button updatePatientForAdmin_button;
+        private TextBox findTextBox_PatientForAdmin;
     }
 }
--- Helsi/PatientForAdminUserControl.cs
+++ Helsi/PatientForAdminUserControl.cs
@@ -26,11 +26,9 @@ namespace Helsi
 
         private void ShowDataToGrit()
         {
-            string query = "select * from Patient";
-
             using (SqlConnection connection = new SqlConnection(GetConectionSrtingForConectDataBase.ConectionString))
             {
-                SqlCommand command = new SqlCommand("GetAllPatients", connection);
+                SqlCommand command = new SqlCommand("GetAllPatientsProc", connection);
                 command.CommandType = CommandType.StoredProcedure;
 
                 SqlDataAdapter adapter = new SqlDataAdapter(command);
@@ -41,7 +39,7 @@ namespace Helsi
             }
         }
 
-        private void showAllDataPatientForAdmin_Button_Click(object sender, EventArgs e)
+        private void findPatientForAdmin_Button_Click(object sender, EventArgs e)
         {
             ShowDataToGrit();
         }
@@ -50,14 +48,166 @@ namespace Helsi
         {
             if (patientForAdmin_dataGridView.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
             {
-                patientForAdmin_dataGridView.CurrentRow.Selected  = true;
+                patientForAdmin_dataGridView.CurrentRow.Selected = true;
+
+                idTextBox_PatientForAdmin.Text = patientForAdmin_dataGridView
+                    .Rows[e.RowIndex]
+                    .Cells["id_patient"]
+                    .FormattedValue.ToString();
 
                 fulnameTextBox_PatientForAdmin.Text = patientForAdmin_dataGridView.
                     Rows[e.RowIndex].
                     Cells["full_name"].
                     FormattedValue.ToString();
+
+                dateBithTextBox_PatientForAdmin.Text = patientForAdmin_dataGridView
+                    .Rows[e.RowIndex]
+                    .Cells["date_of_birth"]
+                    .FormattedValue.ToString();
+
+                phoneNumberTextBox_PatientForAdmin.Text = patientForAdmin_dataGridView
+                    .Rows[e.RowIndex]
+                    .Cells["phone_number"]
+                    .FormattedValue.ToString();
+
+                addressTextBox_PatientForAdmin.Text = patientForAdmin_dataGridView
+                    .Rows[e.RowIndex]
+                    .Cells["address_patient"]
+                    .FormattedValue.ToString();
+
             }
 
         }
+
+        private void addPatientForAdmin_button_Click(object sender, EventArgs e)
+        {
+
+            using (SqlConnection connection = new SqlConnection(GetConectionSrtingForConectDataBase.ConectionString))
+            {
+
+
+                SqlCommand command = new SqlCommand("InsertPatientProc", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+
+                var idUnic = Guid.NewGuid().ToString();
+                //додати параметри
+                command.Parameters.AddWithValue("@id_patient", idUnic);
+                command.Parameters.AddWithValue("@full_name", fulnameTextBox_PatientForAdmin.Text);
+                command.Parameters.AddWithValue("@date_of_bith", dateBithTextBox_PatientForAdmin.Text);
+                command.Parameters.AddWithValue("@phone_number", phoneNumberTextBox_PatientForAdmin.Text);
+                command.Parameters.AddWithValue("@address_patient", addressTextBox_PatientForAdmin.Text);
+
+                connection.Open();
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Пацієнта успішно додано!", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (SqlException ex)
+                {
+                    foreach (SqlError error in ex.Errors)
+                    {
+                        MessageBox.Show(error.Message, "Помилка додавання пацієнта", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Інші неочікувані помилки
+                    MessageBox.Show(ex.Message, "Неочікувана помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+
+
+            }
+            ShowDataToGrit();
+        }
+
+        private void deletePatientForAdmin_button_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConectionSrtingForConectDataBase.ConectionString))
+            {
+
+
+                SqlCommand command = new SqlCommand("DeletePatientProc", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+
+                var idUnic = idTextBox_PatientForAdmin.Text;
+                //додати параметри
+                command.Parameters.AddWithValue("@id_patient", idUnic);
+
+
+                connection.Open();
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Пацієнта успішно видалено!", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (SqlException ex)
+                {
+                    foreach (SqlError error in ex.Errors)
+                    {
+                        MessageBox.Show(error.Message, "Помилка видаленя пацієнта", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Інші неочікувані помилки
+                    MessageBox.Show(ex.Message, "Неочікувана помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                ShowDataToGrit();
+
+            }
+        }
+
+        private void updatePatientForAdmin_button_Click_1(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConectionSrtingForConectDataBase.ConectionString))
+            {
+
+
+                SqlCommand command = new SqlCommand("UpdatePatientProc", connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+
+                var idUnic = idTextBox_PatientForAdmin.Text;
+                //додати параметри
+                command.Parameters.AddWithValue("@id_patient", idUnic);
+                command.Parameters.AddWithValue("@full_name", fulnameTextBox_PatientForAdmin.Text);
+                command.Parameters.AddWithValue("@date_of_bith", dateBithTextBox_PatientForAdmin.Text);
+                command.Parameters.AddWithValue("@phone_number", phoneNumberTextBox_PatientForAdmin.Text);
+                command.Parameters.AddWithValue("@address_patient", addressTextBox_PatientForAdmin.Text);
+
+
+                connection.Open();
+
+                try
+                {
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Пацієнта успішно оновлено!", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (SqlException ex)
+                {
+                    foreach (SqlError error in ex.Errors)
+                    {
+                        MessageBox.Show(error.Message, "Помилка оновлено даних пацієнта", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Інші неочікувані помилки
+                    MessageBox.Show(ex.Message, "Неочікувана помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                ShowDataToGrit();
+
+            }
+        }
+
+       
     }
 }
